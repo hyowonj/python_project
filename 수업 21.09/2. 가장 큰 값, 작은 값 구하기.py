ca0ad7d1a29a68aba5f843c6e80b44deebf5b5a1
hyowonj <93808065+hyowonj@users.noreply.github.com>
2021-12-04 11:52:04 +0900

    Update 2. 가장 큰 값, 작은 값 구하기.py

--- 수업 21.09/2. 가장 큰 값, 작은 값 구하기.py
+++ 수업 21.09/2. 가장 큰 값, 작은 값 구하기.py
@@ -35,27 +35,6 @@
 #     temp_pwd = input("Enter your pwd: ")
 # print("You made it! id:{} pwd:{}".format(temp_id, temp_pwd))
 
-'''
-가장 큰 값 작은 값 구하기
-'''
-import random
-a = []
-for i in range(10):
-    b= random.randrange(1, 110)
-    a.append(b)
-
-maximum_value = a[0]
-for c in range(10):
-    if maximum_value < a[c]:
-        maximum_value = a[c]
-print(a)
-print("가장 큰 값: {}".format(maximum_value))
-
-minimum_value = a[0]
-for d in range(10):
-    if minimum_value > a[d]:
-        minimum_value = a[d]
-print("가장 작은 값: {}".format(minimum_value))
 
 
 a=input()
