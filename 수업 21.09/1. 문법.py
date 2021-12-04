4b11ba08cd29b69f2e830cdeb2a87d1ae0e98987
hyowonj <93808065+hyowonj@users.noreply.github.com>
2021-12-04 11:49:55 +0900

    Update 1. 문법.py

--- 수업 21.09/1. 문법.py
+++ 수업 21.09/1. 문법.py
@@ -4,20 +4,21 @@
 # c=3.5 #float소수
 # print(a)
 # print(b)
+
 # 계산:+-/*% # %:분자
 # print(9%5)
 # print(type(3))
 # print(type("dd"))
 # print(type(3.4))
 # 0:false 1:true
-#
+
 # d=True #bool(T or F)대문자 유의
 # print(type(d))
-#
+
 # e=5
 # if e!=6:   #<(작다) >(크다) ==(같다) !=(같지 않다) <=(크지 않다) >=(작지 읺다)
 #     print(e)
-#
+
 # a=int(input())
 # b="홀수"
 # c="짝수"
@@ -25,7 +26,7 @@
 #     print(c)
 # if a%2!=0:
 #     print(b)
-#
+
 # e=int(input()) #input->문자 int(input())->숫자
 # print(e)
 # for d in range(10):
@@ -34,6 +35,8 @@
 #     print("*"*e)
 # for e in range(1,11):
 #     print("*"*(11-e))
+
+
 # f=int(input())
 # h=7
 # for g in range(2,f):
@@ -45,7 +48,8 @@
 #     print("소수가 아니다")
 # if h==7:
 #     print("소수이다")
-#
+
+
 # for i in range(1,11):
 #     print("*"*(11-i))
 # for i in range(1,11):
@@ -55,7 +59,7 @@
 # if a!=6:
 #     print("haha")
 # print("hihi")
-#
+
 # b=9
 # if b==9:
 #     print("yay")
@@ -63,7 +67,7 @@
 #     print("123")
 # else:
 #     print("0w0")
-#
+
 # d=int(input())
 # f=1
 # for c in range (2,d):
@@ -80,30 +84,28 @@
 #     if h==5:
 #         continue
 #     print(h)
-#
+
 # import random
 # random.randrange(1,7)
 # h=random.randrange(1,7)
 # print(h)
-#
+
 # a=[1,2]
 # print(a)
 # a.append(7)
 # print(a)
 # print(a[0])
-#
-#
+
 # i=0
 # while i < 10:
 #     print("hi",i)
 #     i=i+1
-#
+
 # a=0
 # while a < 10:
 #     print("*"*(a+1))
 #     a=a+1
-#
-#
+
 # for i in range (1,100):
 #
 #     one = i % 10
@@ -117,48 +119,3 @@
 #         print("*")
 #     else:
 #         print(i)
-#
-#
-# id_list=[]
-# pwd_list=[]
-# for e in range (5):
-#     while True:
-#         count=0
-#         temp_id = input("Enter your id: ")
-#         for e in range(len(id_list)):
-#             if id_list[e]==temp_id:
-#                 count =1
-#         if count==0:
-#             print("ok")
-#             break
-#         print("try another one")
-#     id_list.append(temp_id)
-#     temp_pwd=input("Enter your pwd: ")
-#     pwd_list.append(temp_pwd)
-#     print("You made it!! id:{} pwd:{}".format(id_list,pwd_list))
-#
-#
-# id_list=[]
-# pwd_list=[]
-# for e in range (5):
-#     while True:
-#         count=0
-#         temp_id = input("Enter your id: ")
-#         for e in range(len(id_list)):
-#             if id_list[e]==temp_id:
-#                 count =1
-#         if count==0:
-#             print("ok")
-#             break
-#         print("try another one")
-#     id_list.append(temp_id)
-#     temp_pwd=input("Enter your pwd: ")
-#     pwd_list.append(temp_pwd)
-#     print("You made it!! id:{} pwd:{}".format(id_list,pwd_list))
-#
-# a=
-# for a in range (len(a)):
-#     print(a[i])
-#
-# a=4
-# print(len(a))
