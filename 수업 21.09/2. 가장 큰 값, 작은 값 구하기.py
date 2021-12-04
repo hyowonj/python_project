c3855bac79d132495237d9119255f3f36c945ea5
hyowonj <93808065+hyowonj@users.noreply.github.com>
2021-12-04 11:53:34 +0900

    Update 2. 가장 큰 값, 작은 값 구하기.py

--- 수업 21.09/2. 가장 큰 값, 작은 값 구하기.py
+++ 수업 21.09/2. 가장 큰 값, 작은 값 구하기.py
@@ -36,21 +36,6 @@
 # print("You made it! id:{} pwd:{}".format(temp_id, temp_pwd))
 
 
-
-a=input()
-print(len(a))
-
-function, 함수
-
-
-# *설명서
-def cal_sum(x,y):
-    z = x + y
-    return z   #return: 함수를 종료, 값을 반환한다
-result = cal_sum(3,2)
-print(result)
-
-
 import random
 a = []
 def list_random():
