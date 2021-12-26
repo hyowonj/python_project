dfbce2fcb31b8fcd166341ac2af1e3cbc0609c01
hyowonj <93808065+hyowonj@users.noreply.github.com>
2021-12-26 12:23:28 +0900

    Update algorithm_6_소수 찾기.py

--- algorithm/algorithm_6_소수 찾기.py
+++ algorithm/algorithm_6_소수 찾기.py
@@ -1,26 +1,17 @@
+# 1978번(미완성)
 
-'''
-소수 찾기(x)
-'''
-# import random
-#
-# n = int(input())
-# d = list(map(int, input().split(' ')))
-#
-# count = 0
-#
-# for i in range(len(d)):
-#     elm = d[i]
-#     flag = 1
-#     for e in range(2, elm//2 + 1):
-#         if elm % e == 0:
-#             flag += 1
-#             if flag >= 3:
-#                 break
-#     if flag == 2:
-#         count += 1
-#
-# print(count)
+n = int(input())
+d = map(int, input().split(' '))
 
+num_count = 0
 
+for i in d:
+    flag = 0
+    if i > 1:
+        for e in range(2, i):
+            if i % e == 0:
+                flag += 1
+        if flag == 0:
+            num_count += 1
 
+print(flag)
