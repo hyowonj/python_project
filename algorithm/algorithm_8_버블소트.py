b4ab6c32a3940e0d7357bb834cf428139796c484
hyowonj <93808065+hyowonj@users.noreply.github.com>
2021-12-26 12:25:38 +0900

    Update and rename algorithm_8_sorting.py to algorithm_8_버블소트.py

---
+++ algorithm/algorithm_8_버블소트.py
@@ -0,0 +1,20 @@
+# Bubble sort
+import random
+
+def bubble_sort(a):
+    for h in range(len(a)):
+        for e in range(len(a) - 1):
+            if a[e] > a[e + 1]:
+                a[e], a[e + 1] = a[e + 1], a[e]
+                print(a)
+            else:
+                print(a)
+    return(a)
+
+a = []
+for i in range(5):
+    b = random.randrange(1, 200)
+    a.append(b)
+print(a)
+
+a = bubble_sort(a)
