f1c51c84773975ef6ec54f56fbf2f4893bcd7b3c
hyowonj <93808065+hyowonj@users.noreply.github.com>
2021-12-26 12:21:11 +0900

    Update algorithm_1_소수확인.py

--- algorithm/algorithm_1_소수확인.py
+++ algorithm/algorithm_1_소수확인.py
@@ -1,17 +1,17 @@
-d=int(input())
-f=1
-for c in range (2,d):
-    if d%c==0:
-        f=f+1
+d = int(input())
+f = 1
+for c in range (2, d//2 + 1):
+    if d % c == 0:
+        f = f + 1
     else:
-        f=f
-if f!=1:
+        f = f
+if f != 1:
     print("소수가 아니다")
 else:
     print("소수이다")
-for h in range(10):
-    if h==5:
-        continue
-    print(h)
+# for h in range(10):
+#     if h == 5:
+#         continue
+#     print(h)
     
       
